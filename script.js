9c7b59783f007eefd03dcc6b0fdc4aa4fbc2190e
SubatYucel <subat.yucel1@gmail.com>
2024-09-13 15:38:16 +0300

    sort functionality added lesson 153

--- script.js
+++ script.js
@@ -58,9 +58,12 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 //Functions
-function displayMovements(movements) {
+function displayMovements(movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -209,4 +212,12 @@ btnClose.addEventListener('click', function (e) {
     inputClosePin.blur();
     inputCloseUsername.blur();
   }
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  displayMovements(currentAcc.movements, !sorted);
+  sorted = !sorted;
 });
